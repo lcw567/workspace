435812843fddc6c060821c3e97f7a523cc958dea
taeyoung-oh <oty0313@gmail.com>
2024-08-30 16:22:58 +0900

    2408301622

--- 1_JAVA/JavaStudy/src/h/inherit/ex1/Run.java
+++ 1_JAVA/JavaStudy/src/h/inherit/ex1/Run.java
@@ -6,8 +6,8 @@ public class Run {
 //		Man man1 = new Man("최지원");
 //		man1.tellYourName();
 		
-		BusinessMan man2 = new BusinessMan("최지원","KH", "강사");
-		System.out.println(man2.toString());
+		BusinessMan man = new BusinessMan("김개똥","사성", "인턴");
+		man.tellYourInfo();
 	}
 
 }
